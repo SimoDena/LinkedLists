2fae8254d9a4b4cf3b625ce4d82cf9a08d98218b
Simone Denardi <simone.denardi@lasportiva.com>
2022-02-22 17:30:01 +0100

    Aggiungere i file di progetto.

---
+++ LinkedLists/LinkedList.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+        public void AddLast(int value)
+        {
+            Node newNode = new Node(value);
+
+            if(Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+
+            Count++;
+        }
+
+        public void AddFirst(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+
+            Count++;
+        }
+
+        public int IndexOf(int value)
+        {
+            int position = 0;
+            Node nodeToFind = Head;
+            while (nodeToFind != null)
+            {
+                if (nodeToFind.Value == value)
+                {
+                    return position;
+                }
+                else
+                {
+                    nodeToFind = nodeToFind.Next;
+                }
+
+                position++;
+            }
+
+            return -1;
+        }
+
+        public bool Contains(int value)
+        {
+            return IndexOf(value) != -1;
+        }
+
+        public void RemoveFirst()
+        {
+            if (Head == null)
+            {
+                throw new Exception("La lista è vuota!");
+            }
+
+            if (Head == Tail) //La lista ha un solo elemento.
+            {
+                Head = null;
+                Tail = null;
+                return;
+            }
+
+            Node second = Head.Next;
+            Head.Next = null; //Per permmettere al garbage collector di rimuovere l'elemento.
+            Head = second;
+
+            Count--;
+        }
+
+        public void RemoveLast()
+        {
+            if (Head == null)
+            {
+                throw new Exception("La lista è vuota!");
+            }
+
+            if (Head == Tail) //La lista ha un solo elemento.
+            {
+                Head = null;
+                Tail = null;
+                return;
+            }
+
+            Node currentNode = Head;
+            while(currentNode.Next != Tail)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            Tail = currentNode; // La coda è ora il penultimo elemento.
+            Tail.Next = null;
+
+            Count--;
+        }
+
+        public void Reverse()
+        {
+            Node tmp = Head;
+            Node revHead = null;
+            Node prev = null;
+
+            while (tmp != null)
+            {
+                revHead = new Node(0);
+                revHead.Value = tmp.Value;
+                revHead.Next = prev;
+                prev = revHead;
+
+                tmp = tmp.Next;
+            }
+
+            Head = revHead;
+        }
+    }
+}
---
+++ LinkedLists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.AddLast(5);
+            list.AddLast(10);
+            list.AddLast(15);
+            list.AddLast(20);
+
+            //list.AddFirst(5);
+            //list.AddFirst(10);
+            //list.AddFirst(15);
+            //list.AddFirst(20);
+
+            Console.WriteLine(list.IndexOf(15));
+            Console.WriteLine(list.Contains(151));
+
+            //list.RemoveFirst();
+            //list.RemoveLast();
+            list.Reverse();
+            Console.ReadKey();
+        }
+    }
+}
